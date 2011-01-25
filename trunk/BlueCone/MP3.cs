17002910917538caa53f2c417cd207cdd8a51958
krish3525@gmail.com <krish3525@gmail.com@cea9e4c5-3446-5694-582d-99878a3f832b>
2011-01-25 11:45:06 +0000

    initial commit

---
+++ trunk/BlueCone/MP3.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using Microsoft.SPOT;
+
+namespace BlueCone
+{
+    static public class MP3
+    {
+        static private InputPort DREQ;
+
+        static SPI spi;
+        static private SPI.Configuration dataConfig;
+        static private SPI.Configuration cmdConfig;
+
+        // Values
+        const ushort SM_SDINEW = 0x800;
+        const ushort SM_RESET = 0x04;
+
+        // Registers
+        const int SCI_MODE = 0x00;
+        const int SCI_VOL = 0x0B;
+        const int SCI_CLOCKF = 0x03;
+
+        static private byte[] block = new byte[32];
+        static private byte[] cmdBuffer = new byte[4];
+
+        static public void Initialize()
+        {
+            SPI.SPI_module spi_module;
+            spi_module = SPI.SPI_module.SPI1;
+
+            dataConfig = new SPI.Configuration((Cpu.Pin)FEZ_Pin.Digital.Di2, false, 0, 0, false, true, 2000, spi_module);
+            cmdConfig = new SPI.Configuration((Cpu.Pin)FEZ_Pin.Digital.Di9, false, 0, 0, false, true, 2000, spi_module);
+            DREQ = new InputPort((Cpu.Pin)FEZ_Pin.Digital.Di3, false, Port.ResistorMode.PullUp);
+
+            spi = new SPI(dataConfig);
+
+            Reset();
+
+            CommandWrite(SCI_MODE, SM_SDINEW);
+            CommandWrite(SCI_CLOCKF, 0x98 << 8);
+            CommandWrite(SCI_VOL, 0x0101);
+
+            if (CommandRead(SCI_VOL) != (0x0101))
+            {
+                throw new Exception("Failed to initialize MP3 Decoder.");
+            }
+            else
+            {
+                Debug.Print("Mp3 decoder initialized successfully!");
+            }
+
+        }
+
+
+        static public void SetVolume(byte left_channel, byte right_channel)
+        {
+            CommandWrite(SCI_VOL, (ushort)((255 - left_channel) << 8 | (255 - right_channel)));
+        }
+
+        private static void Reset()
+        {
+            while (DREQ.Read() == false) ;
+            CommandWrite(SCI_MODE, (ushort)(CommandRead(SCI_MODE) | SM_RESET));
+            Thread.Sleep(1);
+            while (DREQ.Read() == false) ;
+            Thread.Sleep(100);
+        }
+
+        static private void CommandWrite(byte address, ushort data)
+        {
+            while (DREQ.Read() == false)
+                Thread.Sleep(1);
+
+            spi.Config = cmdConfig;
+            cmdBuffer[0] = 0x02;
+            cmdBuffer[1] = address;
+            cmdBuffer[2] = (byte)(data >> 8);
+            cmdBuffer[3] = (byte)data;
+
+            spi.Write(cmdBuffer);
+
+        }
+
+        static private ushort CommandRead(byte address)
+        {
+            ushort temp;
+
+            while (DREQ.Read() == false)
+                Thread.Sleep(1);
+
+            spi.Config = cmdConfig;
+            cmdBuffer[0] = 0x03;
+            cmdBuffer[1] = address;
+            cmdBuffer[2] = 0;
+            cmdBuffer[3] = 0;
+
+            spi.WriteRead(cmdBuffer, cmdBuffer, 2);
+
+            temp = cmdBuffer[0];
+            temp <<= 8;
+
+            temp += cmdBuffer[1];
+
+            return temp;
+        }
+
+        static public void SendData(byte[] data)
+        {
+            int size = data.Length - data.Length % 32;
+
+            spi.Config = dataConfig;
+            for (int i = 0; i < size; i += 32)
+            {
+
+                while (DREQ.Read() == false)
+                    Thread.Sleep(1);
+
+                Array.Copy(data, i, block, 0, 32);
+
+                spi.Write(block);
+            }
+        }
+    }
+}
